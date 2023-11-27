92fbe07f2702727743cb33d3dcf12bd20a8be215
Duvan Mancilla <Duvanred0@gmail.com>
2023-11-27 15:11:53 -0500

    estructura navList

---
+++ natu/src/components/nav/NavList.jsx
@@ -0,0 +1,33 @@
+function NavList(){
+    return (
+        <>
+        <nav>
+            /*For Mobile */
+            <div className="menuH">
+                <img src="../images/icon/menu.png" alt="" />
+            </div>
+            <ul>
+                <li>
+                    <a href="#">Home</a>
+                </li>
+                <li>
+                    <a href="#">Places</a>
+                </li>
+                <li>
+                    <a href="#">Price</a>
+                </li>
+                <li>
+                    <a href="#">Team</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+        </nav>    
+        </>
+       
+    )
+}
+
+
+export default NavList
